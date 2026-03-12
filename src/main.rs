0881ba5a74da7e0178ab5aef9b5db2945dd7db6e
Aymen Loudiy <258696599+loudiyaymen@users.noreply.github.com>
2026-03-12 13:36:57 -0400

    chore(main): integrate file watching
    Updated main.rs to load WATCH_DIR from config module and start the
    folder watcher.
    
    Temporary testing path is set to current directory.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use config::load_watch_dir;
-
+use watcher::start_watching;
 mod config;
+mod watcher;
 fn main() {
     println!("Hello, world!");
     load_watch_dir();
+    let _ = start_watching(".");
 }
