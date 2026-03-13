2610009fd2bf17647a357308220add5e6a9f82ba
Aymen Loudiy <258696599+loudiyaymen@users.noreply.github.com>
2026-03-13 13:35:19 -0400

    chore(main): integrate encryption module
    Spawn a thread for watcher module
    
    Execute encryption flow
    
    Handle proper Result returns

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use aes_gcm::{aead::OsRng, Aes256Gcm, KeyInit};
 use config::load_watch_dir;
+use encryptor::encrypt_file;
 use watcher::start_watching;
+
 mod config;
+mod encryptor;
 mod watcher;
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> std::io::Result<()> {
     load_watch_dir();
-    let _ = start_watching(".");
+    let watcher_handle = std::thread::spawn(|| {
+        let _ = start_watching(".");
+    });
+    let key = Aes256Gcm::generate_key(OsRng);
+    encrypt_file("test.txt", "encrypted_test.vault", &key)?;
+    watcher_handle.join().unwrap();
+    Ok(())
 }
