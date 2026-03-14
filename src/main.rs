06f01b769a1c50aed95a6032d112e4f8d0070fd7
Aymen Loudiy <258696599+loudiyaymen@users.noreply.github.com>
2026-03-14 13:26:58 -0400

    chore(main): integrate decryption function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use aes_gcm::{aead::OsRng, Aes256Gcm, KeyInit};
 use config::load_watch_dir;
-use encryptor::encrypt_file;
+use encryptor::{decrypt_file, encrypt_file};
 use watcher::start_watching;
 
 mod config;
@@ -14,6 +14,7 @@ fn main() -> std::io::Result<()> {
     });
     let key = Aes256Gcm::generate_key(OsRng);
     encrypt_file("test.txt", "encrypted_test.vault", &key)?;
+    decrypt_file("encrypted_test.vault", &key);
     watcher_handle.join().unwrap();
     Ok(())
 }
