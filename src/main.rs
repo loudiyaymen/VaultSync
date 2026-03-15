f849dd9049fb674cb89bded32f3a4210f1d8a9c6
Aymen Loudiy <258696599+loudiyaymen@users.noreply.github.com>
2026-03-15 12:40:14 -0400

    feat(main): add graceful shutdown via ctrlc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use aes_gcm::{aead::OsRng, Aes256Gcm, KeyInit};
 use config::load_watch_dir;
 use encryptor::{decrypt_file, encrypt_file};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 use watcher::start_watching;
 
 mod config;
@@ -9,12 +13,24 @@ mod watcher;
 
 fn main() -> std::io::Result<()> {
     load_watch_dir();
-    let watcher_handle = std::thread::spawn(|| {
-        let _ = start_watching(".");
+    let shutdown_flag = Arc::new(AtomicBool::new(false));
+
+    let shutdown_handle = shutdown_flag.clone();
+    ctrlc::set_handler(move || {
+        println!("Received Ctrl+C. Shutting down...");
+        shutdown_handle.store(true, Ordering::Relaxed);
+    })
+    .expect("Error setting Ctrl+C handler");
+
+    let watcher_flag = shutdown_flag.clone();
+    let watcher_handle = std::thread::spawn(move || {
+        let _ = start_watching(".", watcher_flag);
     });
+
     let key = Aes256Gcm::generate_key(OsRng);
-    encrypt_file("test.txt", "encrypted_test.vault", &key)?;
-    decrypt_file("encrypted_test.vault", &key);
+    encrypt_file("test.png", &key)?;
+    decrypt_file("test.vault", &key);
+
     watcher_handle.join().unwrap();
     Ok(())
 }
