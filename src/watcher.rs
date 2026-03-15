9a160cca77fa02c7ae521223f08d25eebcd69d4e
Aymen Loudiy <258696599+loudiyaymen@users.noreply.github.com>
2026-03-15 11:49:14 -0400

    feat(watcher): graceful shutdown
    Check shutdown flag inside event loop and exit cleanly on Ctrl+C

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,15 +1,28 @@
-use std::{path::Path, sync::mpsc};
-
 use notify::{recommended_watcher, Event, RecursiveMode, Result, Watcher};
-pub fn start_watching(path: &str) -> Result<()> {
+use std::{
+    path::Path,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        mpsc, Arc,
+    },
+};
+
+pub fn start_watching(path: &str, shutdown: Arc<AtomicBool>) -> Result<()> {
     let (tx, rx) = mpsc::channel::<Result<Event>>();
     let mut watcher = recommended_watcher(tx)?;
     watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
+
     for res in rx {
+        if shutdown.load(Ordering::Relaxed) {
+            println!("Watcher received shutdown signal.");
+            break;
+        }
+
         match res {
-            Ok(event) => println!("Event:{:?}", event),
+            Ok(event) => println!("Event: {:?}", event),
             Err(e) => println!("Watch error: {:?}", e),
         }
     }
+
     Ok(())
 }
